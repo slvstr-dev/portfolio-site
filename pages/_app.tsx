2ed2267edc4a9783c73206928b603a909fd4c60c
slvstr.dev <hello@slvstr.dev>
2021-06-23 20:04:36 +0200

    Add fonts.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.scss";
+import "../styles/style.scss";
 import type { AppProps } from "next/app";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,12 @@ import Head from "next/head";
 import styles from "../styles/Home.module.scss";
 
 const Home = () => (
-	<div className={styles.container}>
+	<div>
 		<Head>
 			<title>slvstr.dev</title>
 		</Head>
 
-		<main className={styles.main}>
+		<main>
 			<h1 className={styles.title}>slvstr.dev</h1>
 		</main>
 	</div>
