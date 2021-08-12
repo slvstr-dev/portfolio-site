b4a2f38af749a61218af0f6c2c6741d708310686
slvstr.dev <hello@slvstr.dev>
2021-08-12 16:52:40 +0200

    Make Error component more flexible.

--- components/blocks/Error.tsx
+++ components/blocks/Error.tsx
@@ -1,15 +1,14 @@
 import Alert from "../../public/svg/alert.svg";
-import useTranslation from "next-translate/useTranslation";
-import styles from "../../shared/styles/components/blocks/Error.module.scss";
+import styles from "../../styles/components/blocks/Error.module.scss";
 
-export const Error: React.FC = () => {
-	const { t } = useTranslation("common");
+interface Error {
+	message: string;
+}
 
-	return (
-		<div className={styles.error}>
-			<Alert className={styles.error__svg} />
+export const Error: React.FC<Error> = ({ message }) => (
+	<div className={styles.error}>
+		<Alert className={styles.error__svg} />
 
-			<p className={styles.error__text}>{t("error_message")}</p>
-		</div>
-	);
-};
+		<p className={styles.error__text}>{message}</p>
+	</div>
+);
