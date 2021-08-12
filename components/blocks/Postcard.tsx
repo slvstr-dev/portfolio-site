6343e526bda384d5bede9f54a2c8f501d8269f6e
slvstr.dev <hello@slvstr.dev>
2021-08-12 16:48:14 +0200

    Implement Sendgrid contact form.

--- components/blocks/Postcard.tsx
+++ components/blocks/Postcard.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import useTranslation from "next-translate/useTranslation";
-import styles from "../../shared/styles/components/blocks/Postcard.module.scss";
+import styles from "../../styles/components/blocks/Postcard.module.scss";
 
 export const Postcard: React.FC = () => {
 	const { t } = useTranslation("contact");
 
+	const [submitNotification, setSubmitNotification] = useState("");
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
 	const [message, setMessage] = useState("");
@@ -20,72 +21,88 @@ export const Postcard: React.FC = () => {
 			message,
 		};
 
-		console.log(formData);
+		fetch("/api/contact", {
+			method: "post",
+			body: JSON.stringify(formData),
+		}).then((response) => {
+			response.status !== 200
+				? setSubmitNotification(t("error_message"))
+				: setSubmitNotification(t("form_submitted"));
+		});
 	};
 
 	return (
 		<section className={styles.postcard}>
 			<div className={styles.postcard__content}>
-				<form className={styles.postcard__form} onSubmit={handleSubmit}>
-					<div className={styles.postcard__message}>
-						<textarea
-							className={styles.postcard__textarea}
-							name={t("form_message")}
-							placeholder={t("form_message_placeholder")}
-							onChange={(e) => setMessage(e.target.value)}
-							required
-						/>
-					</div>
+				{submitNotification ? (
+					<p className={styles.postcard__notification}>
+						{submitNotification}
+					</p>
+				) : (
+					<form
+						className={styles.postcard__form}
+						onSubmit={handleSubmit}
+					>
+						<div className={styles.postcard__message}>
+							<textarea
+								className={styles.postcard__textarea}
+								name={t("form_message")}
+								placeholder={t("form_message_placeholder")}
+								onChange={(e) => setMessage(e.target.value)}
+								required
+							/>
+						</div>
 
-					<div className={styles.postcard__contact}>
-						<span className={styles.postcard__stamp}>
-							SLV
-							<br />
-							STR
-						</span>
-
-						<input
-							className={styles.postcard__input}
-							type="text"
-							name={t("form_name")}
-							placeholder={t("form_name_placeholder")}
-							onChange={(e) => setName(e.target.value)}
-							required
-						/>
-
-						<input
-							className={styles.postcard__input}
-							type="email"
-							name={t("form_email")}
-							placeholder={t("form_email_placeholder")}
-							onChange={(e) => setEmail(e.target.value)}
-							required
-						/>
-
-						<input
-							className={styles.postcard__input}
-							type="tel"
-							name={t("form_phone")}
-							placeholder={t("form_phone_placeholder")}
-							onChange={(e) => setPhone(e.target.value)}
-						/>
-
-						<div className={styles.postcard__buttons}>
-							<button
-								className={styles.postcard__input}
-								type="submit"
-							>
-								{t("form_submit")}
-							</button>
+						<div className={styles.postcard__contact}>
+							<span className={styles.postcard__stamp}>
+								SLV
+								<br />
+								STR
+							</span>
 
 							<input
 								className={styles.postcard__input}
-								type="reset"
-								value={t("form_reset")}
+								type="text"
+								name={t("form_name")}
+								placeholder={t("form_name_placeholder")}
+								onChange={(e) => setName(e.target.value)}
+								required
 							/>
+
+							<input
+								className={styles.postcard__input}
+								type="email"
+								name={t("form_email")}
+								placeholder={t("form_email_placeholder")}
+								onChange={(e) => setEmail(e.target.value)}
+								required
+							/>
+
+							<input
+								className={styles.postcard__input}
+								type="tel"
+								name={t("form_phone")}
+								placeholder={t("form_phone_placeholder")}
+								onChange={(e) => setPhone(e.target.value)}
+							/>
+
+							<div className={styles.postcard__buttons}>
+								<button
+									className={styles.postcard__input}
+									type="submit"
+								>
+									{t("form_submit")}
+								</button>
+
+								<input
+									className={styles.postcard__input}
+									type="reset"
+									value={t("form_reset")}
+								/>
+							</div>
 						</div>
-					</div>
-				</form>
+					</form>
+				)}
 			</div>
 		</section>
 	);
