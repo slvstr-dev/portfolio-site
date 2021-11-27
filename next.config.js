6671f791302bfc01812fbae73e77fa4ff5209320
slvstr.dev <hello@slvstr.dev>
2021-11-27 12:22:59 +0100

    Fix images max-age

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextTranslate = require("next-translate");
 module.exports = nextTranslate({
 	reactStrictMode: true,
 	images: {
-		minimumCacheTTL: 31536000,
 		domains: ["qhvpkodyebxnercuaxvv.supabase.in"],
 	},
 });
--- supabaseClient.js
+++ supabaseClient.js
@@ -2,5 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const options = { headers: { "Cache-Control": "public, max-age=31536000" } };
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, options);
