dc9e9a586a490145c7ada14c1bd6207e3ee404eb
slvstr.dev <hello@slvstr.dev>
2022-06-08 08:11:56 +0200

    Update tailwind config & translations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
 	darkMode: "class",
-	purge: [
+	content: [
 		"./pages/**/*.{js,ts,jsx,tsx}",
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
